b218759f9c94fd330b742c8d4b943ecb6464635d
Joe Grady <Joegrady1@gmail.com>
2015-10-17 13:36:35 -0400

    added style file and added front end point

--- handlers/Home/index.js
+++ handlers/Home/index.js
@@ -1,12 +1,29 @@
+require('./styles.css');
+
 import React from 'react';
+import {apiGet} from 'requestLib' ;
 import {Resolver} from 'react-resolver';
 
 class Home extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {users: [] };
+  }
+
+  componentDidMount(){
+    apiGet("v1/users", {}, (data) => {
+      this.setState({users: data})
+    });
+
+  }
+
 
   render(): ?ReactElement {
     return (
       <div className="Home">
         HOME
+        {this.state.users.map((user) =>{return user.full_name })}
       </div>
     );
   }
