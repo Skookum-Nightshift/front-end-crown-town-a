a4e82fffebac056fed84d898dbbb0f82cd511365
Joe Grady <Joegrady1@gmail.com>
2015-10-18 00:07:06 -0400

    Setup Login

--- routes.js
+++ routes.js
@@ -11,11 +11,15 @@ const {
 import NotFound from './handlers/NotFound';
 import App from './handlers/Base';
 import Home from'./handlers/Home';
+import Login from'./handlers/Login';
+import Error from'./handlers/Error';
 
 var routes = (
   <Route path="/" handler={App} >
     <DefaultRoute name="home" handler={Home} />
     <NotFoundRoute handler={NotFound} />
+    <Route name="login" path="/login" handler={Login} />
+    <Route name="error" path="/error" handler={Error} />
   </Route>
 );
 
